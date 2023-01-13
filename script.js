74d1ecaa979cd2602b5be0bdd352912fc1ecd0a7
Sarah Makkar <sarahmakkar19@gmail.com>
2023-01-13 14:36:31 -0800

    Adding necessary files

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#chatbot-form').submit(function (event) {
+      event.preventDefault();
+      const message = $('#chatbot-input').val();
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/chatbot',
+        // url: 'https://nabil828-comp4537-w23-labs-lab2.onrender.com/chatbot',
+        data: { message: message },
+        success: function (response) {
+          let newMessage = $('<div>', { class: 'message' }).text(response.text);
+          let removeButton = $('<button>', { class: 'remove-button' }).text('Remove');
+          newMessage.append(removeButton);
+          $('#chat-history').append(newMessage);
+        }
+  
+      });
+      $(document).on('click', '.remove-button', function () {
+        $(this).parent().remove();
+      });
+  
+    });
+  });
